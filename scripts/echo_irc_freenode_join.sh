4f001ddf12121254b2997b5b357239d25dda64d8
Robert Fairley <rfairley@users.noreply.github.com>
2019-05-01 09:48:17 -0400

    Create echo_irc_freenode_join.sh

---
+++ scripts/echo_irc_freenode_join.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -euo pipefail
+
+join_cmd="/join #atomic,#cockpit,#coreos,#docker,#fedora-containers,#fedora-coreos,#google-containers,#openshift,#openshift-dev,#announcements,#fedora-releng,#fedora-kernel,#silverblue"
+
+echo "${join_cmd}"
