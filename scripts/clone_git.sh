8881dbf3b48d4bdd7e1092b00665e88a5944b56a
Robert Fairley <19979161+rfairley@users.noreply.github.com>
2019-05-20 17:48:39 -0400

    Specify github user instead of URL to fork

--- scripts/clone_git.sh
+++ scripts/clone_git.sh
@@ -2,18 +2,20 @@
 
 set -exuo pipefail
 
-clone_url=$1
-fork_url=$2
-REPO_LOCATION=/home/rfairley/dev-rfairley/repos/
+# clone_url should be passed as a https:// URL.
+clone_url="${1}"
+github_user="${2}"
 
 domain=$(echo $clone_url | awk -F/ '{print $3}')
 user=$(echo $clone_url | awk -F/ '{print $4}')
 name=$(echo $clone_url | awk -F/ '{print $5}')
 
-cd $REPO_LOCATION
+# Always want to use SSH from the $github_user's fork.
+fork_url="git@github.com:${github_user}/${name}"
+
+cd $RFAIRLEY_REPOS
 mkdir -p $domain/$user
 cd $domain/$user
 git clone $clone_url
 cd $name
-git remote add $USER $fork_url
-
+git remote add "${github_user}" "${fork_url}"
